1520e5d76d288b99e7adc7513a0c4ef20b2c9269
tomascalquin <tcalquin@alumnos.uai.cl>
2026-05-24 20:36:43 -0400

    liosto

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Reemplaza esto con los datos reales de tu proyecto en Supabase
-const supabaseUrl = 'https://fwhjzjqkvfdsezfgpqdi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3aGp6anFrdmZkc2V6ZmdwcWRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIxNjY2MTMsImV4cCI6MjA4Nzc0MjYxM30.1MLuKDs61KF1XqOBdp_TFmOK6taP3pbOpsro75zA_rI'
+// Extraemos las llaves de las variables de entorno de Vite
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
